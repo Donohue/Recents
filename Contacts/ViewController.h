59515cc57310d11ff6114d9693ba83ea13a8a25f
Brian Donohue <brian.donohue89@gmail.com>
2015-06-13 11:04:45 -0700

    Initial Commit

---
+++ Contacts/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Contacts
+//
+//  Created by Brian Donohue on 6/13/15.
+//  Copyright (c) 2015 Brian Donohue. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Contacts/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  Contacts
+//
+//  Created by Brian Donohue on 6/13/15.
+//  Copyright (c) 2015 Brian Donohue. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
